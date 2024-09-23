a0d42ed80840f02bb6aa82f8c688bf27423ea962
Ulugbek Abdullaev <ulugbekna@gmail.com>
2024-09-23 18:49:21 +0200

    add support for viewing strings in JSONL objects

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,14 @@
-import * as vscode from "vscode";
 import * as Parser from "jsonc-parser";
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.languages.registerHoverProvider("json", new JsonStringHoverProvider())
+	);
+
+	context.subscriptions.push(
+		vscode.languages.registerHoverProvider("jsonl", new JsonLStringHoverProvider())
 	);
 }
 
@@ -26,7 +30,39 @@ class JsonStringHoverProvider implements vscode.HoverProvider {
 		const strRange = new vscode.Range(start, end);
 		const str = document.getText(strRange);
 
-		if (!str.match(/\\r|\\n|\\r\\n/)) { return; }
+		if (!str.match(/\\r|\\n|\\r\\n/) && str.length < 20) { return; }
+
+		const unquotedStr = str.slice(1, -1);
+
+		const whitespaceFixedStr = unquotedStr.replace(/\\n/g, '\n').replace(/\\t/g, '	');
+
+		return new vscode.Hover(
+			new vscode.MarkdownString(`\`\`\`markdown\n${whitespaceFixedStr}\n\`\`\``),
+			strRange
+		);
+	}
+}
+
+class JsonLStringHoverProvider implements vscode.HoverProvider {
+
+	provideHover(document: vscode.TextDocument, position: vscode.Position): vscode.Hover | undefined {
+
+		const line = document.lineAt(position.line);
+		const docContents = line.text;
+		const parseTree = Parser.parseTree(docContents);
+
+		if (parseTree === undefined) { return; }
+
+		const nodeAtCursor = Parser.findNodeAtOffset(parseTree, position.character);
+
+		if (nodeAtCursor === undefined || nodeAtCursor.type !== 'string') { return; }
+
+		const start = document.positionAt(nodeAtCursor.offset);
+		const end = document.positionAt(nodeAtCursor.offset + nodeAtCursor.length);
+		const strRange = new vscode.Range(start, end);
+		const str = document.getText(strRange);
+
+		if (!str.match(/\\r|\\n|\\r\\n/) && str.length < 20) { return; }
 
 		const unquotedStr = str.slice(1, -1);
 
